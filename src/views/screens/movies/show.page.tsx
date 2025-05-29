7c9924e5a19e1665ef4d4d56d82795da78ac3aff
mochamadyudi <myudisobari12@gmail.com>
2025-05-29 20:44:39 +0700

    Adjustment page using partials components

--- src/views/screens/movies/show.page.tsx
+++ src/views/screens/movies/show.page.tsx
@@ -1,120 +1,168 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {ITmdbState} from "@state/reducers/tmdb.ts";
-import {useParams} from "react-router-dom";
-import {GetMovieDetail} from "@state/actions/tmdb.ts";
-import TheContainer from "@components/layouts/TheContainer.tsx";
-import {Button, Divider, Flex, FloatButton, Input, Rate, Typography} from "antd";
-import {Icons} from "@components/atoms/Icons.tsx";
-import {formatNumberShort} from "@utils/index.ts";
-import HttpUtil from "@utils/http.util.ts";
-import CastingSection from "@partials/MoviesDetail/casting.section.tsx";
-import MediaSection from "@partials/MoviesDetail/media.section.tsx";
-import RecommendationsSection from "@partials/MoviesDetail/recommendations.section.tsx";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { ITmdbState } from '@state/reducers/tmdb.ts'
+import { useParams } from 'react-router-dom'
+import { GetMovieDetail } from '@state/actions/tmdb.ts'
+import TheContainer from '@components/layouts/TheContainer.tsx'
+import {
+  Button,
+  Divider,
+  Flex,
+  FloatButton,
+  Rate,
+  Result,
+  Typography,
+} from 'antd'
+import { Icons } from '@components/atoms/Icons.tsx'
+import { formatNumberShort } from '@utils/index.ts'
+import HttpUtil from '@utils/http.util.ts'
+import CastingSection from '@partials/MoviesDetail/casting.section.tsx'
+import MediaSection from '@partials/MoviesDetail/media.section.tsx'
+import RecommendationsSection from '@partials/MoviesDetail/recommendations.section.tsx'
+import MovieCategoryTab from '@partials/Home/movie-category.tab.tsx'
+import ContentLoading from '@components/molecul/loading/content.loading.tsx'
+import dayjs from 'dayjs'
 
-const desc = ['terrible', 'bad', 'normal', 'good', 'wonderful'];
+const desc = ['terrible', 'bad', 'normal', 'good', 'wonderful']
 const Page: React.FC = () => {
-  const param = useParams<'movieId'>();
-  const dispatch = useDispatch();
-  const languages = useSelector<any, ITmdbState['languages']>((state) => state.tmdb.languages);
-  const state = useSelector<any, ITmdbState['movies']['show']>((state) => state.tmdb.movies.show) as ITmdbState['movies']['show'];
+  const param = useParams<'movieId'>()
+  const dispatch = useDispatch()
+  const languages = useSelector<any, ITmdbState['languages']>(
+    (state) => state.tmdb.languages
+  )
+  const state = useSelector<any, ITmdbState['movies']['show']>(
+    (state) => state.tmdb.movies.show
+  ) as ITmdbState['movies']['show']
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [param]);
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [param])
 
   useEffect(() => {
-    if(param.hasOwnProperty('movieId') && param.movieId){
-      if(!isNaN(parseInt(param?.movieId))){
-        dispatch(GetMovieDetail({
-          movieId: parseInt(param.movieId),
-          params: {
-            language: languages.current?.iso_639_1 ?? 'en-US'
-          }
-        }))
+    if (param.hasOwnProperty('movieId') && param.movieId) {
+      if (!isNaN(parseInt(param?.movieId))) {
+        dispatch(
+          GetMovieDetail({
+            movieId: parseInt(param.movieId),
+            params: {
+              language: languages.current?.iso_639_1 ?? 'en-US',
+            },
+          })
+        )
       }
     }
-  }, [param, languages.current]);
+  }, [param, languages.current])
 
   return (
     <React.Fragment>
-      <div className="w-full app-content-full app-page-pt flex flex-col relative overflow-hidden">
-        <section className="flex flex-col items-center pt-10 flex-1">
-          <TheContainer className="z-10 space-y-20 flex flex-col flex-1">
-            <div className="space-y-10 flex-1">
-              <div className="w-1/4">
-                <Input
-                  prefix={<Icons type={'SearchOutlined'} className="text-base"/>}
-                  placeholder="Search..."
-                  size="large"
-                  classNames={{
-                    input: "placeholder:text-white !text-white !text-base"
-                  }}
-                  className="!bg-white bg-opacity-20 border-none"
-                />
-              </div>
-              <Typography.Title level={1}
-                                className="text-7xl">{state.loading ? 'Loading...' : state.data?.title}</Typography.Title>
-              <Typography className="text-2xl">{state.data?.overview}</Typography>
+      <div className="w-full app-content-full app-page-pt flex flex-col overflow-hidden relative">
+        {state?.loading ? (
+          <ContentLoading data-testid="loading-spinner" />
+        ) : state.data !== null ? (
+          <React.Fragment>
+            <section className="flex flex-col items-center pt-10 flex-1">
+              <TheContainer className="z-10 pt-20 space-y-20 flex flex-col flex-1">
+                <div className="space-y-10 flex-1">
+                  <Typography.Title level={1} className="text-7xl">
+                    {state.loading ? 'Loading...' : state.data?.title}
+                  </Typography.Title>
+                  <Typography className="text-2xl">
+                    {state.data?.overview}
+                  </Typography>
+                  <Typography className="text-2xl">
+                    {state.data?.tagline}
+                  </Typography>
 
-              <Flex gap={10} align="center">
-                {
-                  state.data?.vote_average &&
-                  !isNaN(state.data?.vote_average) && (
-                    <React.Fragment>
-                      <Rate
-                        disabled
-                        defaultValue={Math.floor(state.data.vote_average) / 2}
-                        value={Math.floor(state.data.vote_average) / 2} tooltips={desc}
-                      />
-                      <Divider type="vertical"/>
-                    </React.Fragment>
-                  )
-                }
+                  <Flex gap={10} align="center">
+                    {state.data?.vote_average &&
+                      !isNaN(state.data?.vote_average) && (
+                        <React.Fragment>
+                          <Rate
+                            disabled
+                            defaultValue={
+                              Math.round((state.data.vote_average / 2) * 2) / 2
+                            }
+                            value={
+                              Math.round((state.data.vote_average / 2) * 2) / 2
+                            }
+                            tooltips={desc}
+                          />
+                          <Divider type="vertical" />
+                        </React.Fragment>
+                      )}
 
-                {
-                  state.data?.popularity &&
-                  !isNaN(state.data?.popularity) && (
-                    <span className="flex items-center gap-1">
-                      <Icons type={'HeartFilled'} className="text-rose-500"/>
-                      <Typography>{formatNumberShort(state.data.popularity)}</Typography>
-                    </span>
-                  )
+                    {state.data?.popularity &&
+                      !isNaN(state.data?.popularity) && (
+                        <span className="flex items-center gap-1">
+                          <Icons
+                            type={'HeartFilled'}
+                            className="text-rose-500"
+                          />
+                          <Typography>
+                            {formatNumberShort(state.data.popularity)}
+                          </Typography>
+                        </span>
+                      )}
+                  </Flex>
+                  {
+                    state.data?.status === 'Released' && (
+                      <Typography>
+                        Release {dayjs(state?.data?.release_date, 'YYYY-MM-DD').isValid() ? dayjs(state?.data?.release_date, 'YYYY-MM-DD').format('DD MMMM, YYYY'): '-'}
+                      </Typography>
+                    )
+                  }
+
+                  <Flex gap={10}>
+                    <Button
+                      size="large"
+                      type="primary"
+                      shape="round"
+                      className="px-10 flex items-center text-base"
+                      icon={
+                        <Icons
+                          type={'PlayCircleOutlined'}
+                          className="h-6 text-lg"
+                        />
+                      }
+                    >
+                      Watching
+                    </Button>
+                    <Button size="large" type="primary" ghost>
+                      Trailer
+                    </Button>
+                  </Flex>
+                </div>
+              </TheContainer>
+            </section>
+            <div className="absolute inset-0 bg-black bg-opacity-20 z-[1]" />
+            <div className="absolute bottom-0 left-0 right-0 h-48 bg-gradient-to-t from-black to-transparent z-[1]" />
+            <div className="absolute top-0 left-0 w-full app-content-full z-0">
+              <img
+                src={
+                  HttpUtil.getAssetTmdb(
+                    state.data?.backdrop_path,
+                    'original'
+                  ) as string
                 }
-              </Flex>
-              <Flex gap={10}>
-                <Button
-                  size="large"
-                  type="primary"
-                  shape="round"
-                  className="px-10 flex items-center text-base"
-                  icon={<Icons type={'PlayCircleOutlined'} className="h-6 text-lg"/>}
-                >Watching</Button>
-                <Button
-                  size="large"
-                  type="primary"
-                  ghost>Trailer</Button>
-              </Flex>
+                alt="backdrop-image"
+                loading="lazy"
+                className="relative w-full h-full inset-0 object-cover brightness-[0.3]"
+              />
             </div>
-          </TheContainer>
-        </section>
-        <div className="absolute inset-0 bg-black bg-opacity-20 z-[1]"/>
-        <div className="absolute bottom-0 left-0 right-0 h-48 bg-gradient-to-t from-black to-transparent z-[1]"/>
-        <div className="absolute top-0 left-0 w-full app-content-full z-0">
-          <img
-            src={HttpUtil.getAssetTmdb(state.data?.backdrop_path, 'original') as string}
-            alt="backdrop-image"
-            loading="lazy"
-            className="relative w-full h-full inset-0 object-cover brightness-[0.3]"
-          />
-        </div>
+          </React.Fragment>
+        ) : (
+          <div className="flex-1 flex items-center justify-center">
+            <Result status="404" title="Movie not found" />
+          </div>
+        )}
       </div>
-      <CastingSection/>
-      <MediaSection/>
-      <RecommendationsSection/>
-      <FloatButton.BackTop/>
+      <CastingSection />
+      <MediaSection />
+      <RecommendationsSection />
+      <MovieCategoryTab />
+      <FloatButton.BackTop />
     </React.Fragment>
   )
-};
+}
 
-export default Page;
+export default Page
