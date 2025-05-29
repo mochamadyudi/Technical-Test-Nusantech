b6cd65a0a6760eaf43d9f5be69281b15e8994aea
mochamadyudi <myudisobari12@gmail.com>
2025-05-29 20:42:15 +0700

    Adding props general on loading component

--- src/components/molecul/loading/content.loading.tsx
+++ src/components/molecul/loading/content.loading.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 import LottiesMolecul from '../lotties.molecul.tsx'
 import Animation from "../../../assets/lotties/loading-spinner-dots.json"
 
-const ContentLoading: React.FC = () => {
+const ContentLoading: React.FC<any> = (props) => {
   return (
     <React.Fragment>
-      <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center">
+      <div className="absolute top-0 left-0 h-full w-full flex items-center justify-center" {...props}>
         <LottiesMolecul
           width={250}
           height={250}
