51158073a4974e606fe29d30d3d830c0e7475574
mochamadyudi <myudisobari12@gmail.com>
2025-05-29 20:44:10 +0700

    adding new prop

--- src/components/atoms/generals/card.movie.tsx
+++ src/components/atoms/generals/card.movie.tsx
@@ -1,56 +1,55 @@
-import React from 'react';
-import HttpUtil from "../../../utils/http.util.ts";
-import {TmdbMovieItem} from "@state/reducers/tmdb.ts";
-import {Classes, formatNumberShort} from "../../../utils";
-import {Typography} from "antd";
-import dayjs from "dayjs";
-import {Icons} from "@components/atoms/Icons.tsx";
-import {Link} from "react-router-dom";
+import React from 'react'
+import HttpUtil from '../../../utils/http.util.ts'
+import { TmdbMovieItem } from '@state/reducers/tmdb.ts'
+import { Classes, formatNumberShort } from '../../../utils'
+import { Typography } from 'antd'
+import dayjs from 'dayjs'
+import { Icons } from '@components/atoms/Icons.tsx'
+import { Link } from 'react-router-dom'
 
 type CardCommonProps = {
-  className?: string;
+  className?: string
   classNames?: {
-    root?: string;
-    content?: string;
+    root?: string
+    content?: string
   }
-  style?: React.CSSProperties;
-  testId?: string;
+  style?: React.CSSProperties
+  testId?: string
 }
 
 type CardPost<T = TmdbMovieItem | any> = {
-  type: 'card-post';
-  ratings?: boolean;
-  data: T;
-} & CardCommonProps;
+  type: 'card-post'
+  ratings?: boolean
+  data: T
+} & CardCommonProps
 type CardImageProps<T = TmdbMovieItem | any> = {
-  type: 'card-image';
-  data: T;
-} & CardCommonProps;
+  type: 'card-image'
+  data: T
+} & CardCommonProps
 
-type ICardMovieProps = CardPost<TmdbMovieItem> | CardImageProps<TmdbMovieItem>;
+type ICardMovieProps = CardPost<TmdbMovieItem> | CardImageProps<TmdbMovieItem>
 
-const CardMovie: React.FC<ICardMovieProps> = ({data, type, ...props}) => {
+const CardMovie: React.FC<ICardMovieProps> = ({ data, type, ...props }) => {
   return (
     <React.Fragment>
-      <div
-        data-testid={props?.testId ?? `movie-${type}`}
-        className={Classes(
-          "w-full flex flex-col",
-          props?.classNames?.root,
-        )}
-        style={props?.style ?? {}}
-      >
-        <Link to={`/movies/${data.id}`}>
+      <Link to={`/movies/${data.id}`} style={{ cursor: 'pointer !important'}}>
+        <div
+          data-testid={props?.testId ?? `movie-${type}`}
+          className={Classes('w-full flex flex-col', props?.classNames?.root)}
+          style={props?.style ?? {}}
+        >
           <div className="overflow-hidden rounded-xl">
             <img
-              src={HttpUtil.getAssetTmdb(data?.poster_path, {
-                width: 150,
-                height: 150,
-                platform: 'bestv2'
-              }) ?? ''}
+              src={
+                HttpUtil.getAssetTmdb(data?.poster_path, {
+                  width: 150,
+                  height: 150,
+                  platform: 'bestv2',
+                }) ?? ''
+              }
               alt={`movie_poster`}
               className={Classes(
-                'w-full h-full object-cover relative',
+                'w-full h-full object-cover relative select-none',
                 props?.className
               )}
               srcSet={[
@@ -59,46 +58,45 @@ const CardMovie: React.FC<ICardMovieProps> = ({data, type, ...props}) => {
               loading="lazy"
             />
           </div>
-        </Link>
-        {
-          type === "card-post" && (
-            <div className={Classes(
-              "w-full pb-4 flex flex-col h-auto",
-              props?.classNames?.content,
-            )}>
+          {type === 'card-post' && (
+            <div
+              className={Classes(
+                'w-full pb-4 flex flex-col h-auto',
+                props?.classNames?.content
+              )}
+            >
               <div className="h-10 pt-2">
-                <Typography className="truncate text-lg text-white !font-bold !font-pro-bold">{data.title}</Typography>
+                <Typography className="truncate text-lg text-white !font-bold !font-pro-bold">
+                  {data.title}
+                </Typography>
               </div>
               <div className="flex items-center justify-between">
-                <Typography>{dayjs(data.release_date, 'YYYY-MM-DD').year()}</Typography>
+                <Typography>
+                  {dayjs(data.release_date, 'YYYY-MM-DD').year()}
+                </Typography>
                 <div className="flex items-center gap-2">
-                  {
-                    data?.popularity &&
-                    !isNaN(data?.popularity) && (
-                      <span className="flex items-center gap-1">
-                      <Icons type={'HeartFilled'} className="text-rose-500"/>
-                      <Typography>{formatNumberShort(data.popularity)}</Typography>
+                  {data?.popularity && !isNaN(data?.popularity) && (
+                    <span className="flex items-center gap-1">
+                      <Icons type={'HeartFilled'} className="text-rose-500" />
+                      <Typography>
+                        {formatNumberShort(data.popularity)}
+                      </Typography>
                     </span>
-                    )
-                  }
-                  {
-                    data?.vote_average &&
-                    !isNaN(data?.vote_average) && (
-                      <span className="flex items-center gap-1">
-                      <Icons type={'StarFilled'} className="text-amber-400"/>
+                  )}
+                  {data?.vote_average && !isNaN(data?.vote_average) && (
+                    <span className="flex items-center gap-1">
+                      <Icons type={'StarFilled'} className="text-amber-400" />
                       <Typography>{data.vote_average.toFixed(1)}</Typography>
                     </span>
-                    )
-                  }
+                  )}
                 </div>
-
               </div>
             </div>
-          )
-        }
-      </div>
+          )}
+        </div>
+      </Link>
     </React.Fragment>
   )
-};
+}
 
-export default CardMovie;
+export default CardMovie
